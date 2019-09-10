5e9bee8742966c153c60e9c33ec78c2fb1fa895c
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-09-10 20:23:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/model/Pokemon.kt
@@ -0,0 +1,7 @@
+package br.com.fiap.model
+
+data class Pokemon(
+    val number: String,
+    val name: String,
+    val imageURL: String
+)
---
+++ app/src/main/java/br/com/fiap/repository/PokemonRepository.kt
@@ -0,0 +1,18 @@
+package br.com.fiap.repository
+
+import br.com.fiap.model.Pokemon
+
+interface PokemonRepository {
+    fun checkHealth(
+        onComplete:() -> Unit,
+        onError: (Throwable?) -> Unit
+    )
+
+    fun getPokemons(
+        size: Int,
+        sort: String,
+        onComplete: (List<Pokemon>?) -> Unit,
+        onError: (Throwable?) -> Unit
+    )
+
+}
